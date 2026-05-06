0dd422a2a76a256021f6a7386b919e7085d23997
GregMarkitan <s26387@pjwstk.edu.pl>
2026-05-06 13:59:51 +0200

    Upload the created Placeholder code

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+  Placeholder code:
+  - This section will be replaced later
+  - Used for initial project structure
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+
+    }
+}
